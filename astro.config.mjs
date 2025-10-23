908e780a336dd150302a682b4989cb093ef70de3
conniemzhang <cmzhang96@gmail.com>
2025-10-23 15:34:31 -0400

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,32 +7,33 @@ import robotsTxt from "astro-robots-txt";
 import { loadEnv } from "vite";
 
 const { SITE_URL, APP_ENV } = loadEnv(
-  process.env.NODE_ENV || "development",
-  process.cwd(),
-  "",
+    process.env.NODE_ENV || "development",
+    process.cwd(),
+    "",
 );
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE_URL || "https://astro-starter.piry.dev",
-  integrations: [
-    sitemap({
-      changefreq: "monthly",
-      priority: 0.7,
-      lastmod: new Date(),
-    }),
-    robotsTxt({
-      policy:
-        APP_ENV === "production"
-          ? [{ userAgent: "*", allow: "/" }]
-          : [{ userAgent: "*", disallow: "/" }],
-    }),
-  ],
-  vite: {
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+    site: 'https://conniezhang.github.io/website/',
+    base: '/',
+    integrations: [
+        sitemap({
+            changefreq: "monthly",
+            priority: 0.7,
+            lastmod: new Date(),
+        }),
+        robotsTxt({
+            policy:
+                APP_ENV === "production"
+                    ? [{ userAgent: "*", allow: "/" }]
+                    : [{ userAgent: "*", disallow: "/" }],
+        }),
+    ],
+    vite: {
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
+        },
     },
-  },
 });
