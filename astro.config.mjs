af12548323f1af300a76ea128c2c7138fef17460
conniemzhang <cmzhang96@gmail.com>
2025-10-23 15:14:55 -0400

    switching to astro

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { fileURLToPath, URL } from "node:url";
+
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import { loadEnv } from "vite";
+
+const { SITE_URL, APP_ENV } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+);
+
+// https://astro.build/config
+export default defineConfig({
+  site: SITE_URL || "https://astro-starter.piry.dev",
+  integrations: [
+    sitemap({
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+    robotsTxt({
+      policy:
+        APP_ENV === "production"
+          ? [{ userAgent: "*", allow: "/" }]
+          : [{ userAgent: "*", disallow: "/" }],
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+});
--- content/posts/my-first-post.md
+++
@@ -1,5 +0,0 @@
-+++
-title = 'My First Post'
-date = 2024-03-22T22:52:27-04:00
-draft = true
-+++
